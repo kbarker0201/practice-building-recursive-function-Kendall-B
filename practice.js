96262654b6235efd9d7aa56788272719f9d5d281
kbarker0201 <kbarker0201@gmail.com>
2025-08-10 20:28:09 -0500

    final changes

--- practice.js
+++ practice.js
@@ -1,1 +1,11 @@
-blah
+function reverseString(string) {
+    if (string.length === 1) { //Base case
+        return string;
+    }
+    return string[string.length - 1] + reverseString(string.slice(0, -1)); //Recursive case
+}
+
+console.log(reverseString("Hello"));
+console.log(reverseString("Kendall"));
+console.log(reverseString("LaunchCode"));
+console.log(reverseString("A"));
